55144399cb1f75905ff9efe9fdda393aaac13e99
renan04lima <renan04lima@outlook.com>
2024-04-24 13:09:58 -0300

    fix: move login path to /

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import { AuthProvider } from "./hooks/useAuth";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
+    <BrowserRouter>
+      <AuthProvider>
         <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route index element={<Products />} path="/products" />
-          </Route>
           <Route index element={<Login />} path="/" />
           <Route element={<SignUp />} path="/sign-up" />
+          <Route element={<PrivateRoutes />}>
+            <Route element={<Products />} path="/products" />
+          </Route>
         </Routes>
-      </Router>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
